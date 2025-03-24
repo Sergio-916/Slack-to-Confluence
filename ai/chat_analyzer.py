97b0570f70f527a242dc6f56d1fbf006321a1b49
Sergey Shpak <sergey.shpak79@gmail.com>
2025-03-24 16:42:13 +0300

    add code snippet generation

--- ai/chat_analyzer.py
+++ ai/chat_analyzer.py
@@ -12,7 +12,18 @@ class ChatAnalyzer:
         prompt = """You are a helpful assistant that can analyze messages from slack channel and 
         create document with all relative data and information for confluence knowledge base.
         Convert messages to HTML format. Use only <h1>, <h2>, <h3>, <p>, <ul>, <a>, <b>, <i> tags.
-        Output format: <h1>Title</h1><p>Message</p>"""
+        Output format: <h1>Title</h1><p>Message</p>
+        - if you need to add code snippet use following format 
+        <ac:structured-macro ac:name="code">
+        <ac:parameter ac:name="language">python</ac:parameter> <!-- язык опционально -->
+        <ac:plain-text-body><![CDATA[
+        def hello_world():
+         print("Hello, world!")
+          ]]></ac:plain-text-body>
+            </ac:structured-macro>
+            example: <h1>Example</h1><ac:structured-macro ac:name=\"code\"><ac:parameter ac:name=\"language\">python</ac:parameter><ac:plain-text-body><![CDATA[\ndef hello():\n    print(\"Hello, world!\")\n]]></ac:plain-text-body></ac:structured-macro>
+
+"""
         messages_text = "\n".join(
             [msg.get("text", "") for msg in messages if msg.get("text")][::-1]
         )
